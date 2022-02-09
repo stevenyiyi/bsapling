9e797a71e0a371dabf85e396dadd2a2c8e18125b
stevenyiyi <104037806@qq.com>
2022-02-09 22:10:47 +0800

    afasdfasdf

--- src/config.js
+++ src/config.js
@@ -1,8 +1,12 @@
 export default {
-  apiBaseUrl: process.env.PROD
-    ? "https://a.anylooker.com/"
-    : "https://localhost/",
-  resBaseUrl: process.env.PROD ? "https://anylooker.com" : "https://localhost/",
+  apiBaseUrl:
+    process.env.NODE_ENV === "production"
+      ? "https://a.anylooker.com/"
+      : "https://localhost/",
+  resBaseUrl:
+    process.env.NODE_ENV === "production"
+      ? "https://anylooker.com"
+      : "https://localhost/",
   googleWebClientId:
     "947434104376-39t4p666elc7s9tlc5ac7hnec6g3oips.apps.googleusercontent.com"
 };

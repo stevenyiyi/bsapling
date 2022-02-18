a41822ccb5c4cfbbb207da7c2d6e62a9d9b6b04d
stevenyiyi <104037806@qq.com>
2022-02-18 11:46:36 +0800

    fasdfasdfa

--- src/user_context.js
+++ src/user_context.js
@@ -6,27 +6,30 @@ export const userCookies = {
   username: Cookies.get("username"),
   token: Cookies.get("token"),
   role: Cookies.get("role"),
-  is_login: (async () => {
+  is_login: (() => {
     if (!Cookies.get("username")) {
       return false;
     }
     let qparams = { ts: Date.now() };
     qparams.username = Cookies.get("username");
     qparams.token = Cookies.get("token");
-    await http
+    let is_login = false;
+    http
       .get(path, { params: qparams })
       .then((response) => {
         let result = response.data.result;
         if (result === 0) {
-          return true;
+          is_login = true;
         } else {
-          return false;
+          is_login = false;
         }
       })
+      .then((result) => result)
       .catch((e) => {
         console.log("Error:", e);
-        return false;
+        is_login = false;
       });
+    return is_login;
   })()
 };
 export const UserContext = React.createContext({

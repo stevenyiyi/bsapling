5e8cebffd5cfbd937ab1f168f5bf6ce25fdafad6
stevenyiyi <104037806@qq.com>
2023-08-07 17:10:57 +0800

    fasdfas

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  apiBaseUrl: "10.201.2.17",
+  apiBaseUrl: "http://10.201.2.17/b/",
   googleWebClientId:
     "947434104376-39t4p666elc7s9tlc5ac7hnec6g3oips.apps.googleusercontent.com"
 };

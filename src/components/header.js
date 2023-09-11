7b95f7d3990fc34906e3ce3b8de2fd5aaed2bdc9
stevenyiyi <104037806@qq.com>
2023-09-11 15:15:28 +0800

    sdaffdsafasf

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,11 @@ import "./sidebar.css";
 const header = () => {
   return (
     <div className="sidebar">
+      <NavLink to="/my_schools">学校信息</NavLink>
+      <NavLink to="/my_teachers">教师信息</NavLink>
+      <NavLink to="my_classes">班级信息</NavLink>
+      <NavLink to="my_students">学生信息</NavLink>
+      <NavLink to="recipe_info">食谱信息</NavLink>
       <NavLink to="/my_devices">我的设备</NavLink>
       <NavLink to="/my_subusers">我的用户</NavLink>
     </div>
